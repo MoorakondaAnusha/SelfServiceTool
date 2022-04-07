26fb7b9f2f58364d4c15e96b02a09f2e176b92f6
MoorakondaAnusha <mooanusha@deloitte.com>
2022-04-07 16:33:55 +0530

    updated on 04072022

--- Controllers/SSTController.cs
+++ Controllers/SSTController.cs
@@ -7,6 +7,9 @@ using System.Threading.Tasks;
 using System.Net.Http;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
+using System.Text;
+using Newtonsoft.Json.Linq;
+using System.Collections.ObjectModel;
 
 namespace CrudCoreDatabaseFirst.Controllers
 {
@@ -47,16 +50,59 @@ namespace CrudCoreDatabaseFirst.Controllers
 
             var url = $"{configuration["QKBEndpoint"]}/language/query-knowledgebases/projects/SCAL/qnas?api-version=2021-10-01";
 
-            var JsonResponse = await CallApiEndpoint(url);
+            var JsonResponse = await CallApiEndpoint(url, "get", string.Empty);
 
             allQnAs = JsonConvert.DeserializeObject<QnARecords>(JsonResponse);
 
-            for (var i = 0; i < allQnAs.QnAs.Length; i++)
-            {
-                allQnAs.QnAs[i].Metadata = JsonConvert.DeserializeObject<KeyValuePair<string, string>[]>(JsonResponse);
-            }
-
             return allQnAs;
+        }
+
+        /// <summary>
+        /// Wrapper method to call QueryKB API endpoint
+        /// </summary>
+        [HttpPost]
+        [Route("api/QueryKB")]
+        public async Task<QnARecords> QueryKB()
+        {
+            QnARecords qnARecord = new QnARecords();
+            SuggestedQuestions question = new SuggestedQuestions();
+            question.Question = "location";
+            var content = JsonConvert.SerializeObject(question);
+
+            var url = $"{configuration["QKBEndpoint"]}/language/:query-knowledgebases?projectName=SCAL&api-version=2021-10-01&deploymentName=test";
+
+            var JsonResponse = await CallApiEndpoint(url, "post", content);
+
+            qnARecord = JsonConvert.DeserializeObject<QnARecords>(JsonResponse);
+
+            return qnARecord;
+        }
+
+        /// <summary>
+        /// Wrapper method to call UpdateQnAs API endpoint
+        /// </summary>
+        [HttpPatch]
+        [Route("api/UpdateQnAs")]
+        public async Task<string> UpdateQnAs()
+        {
+            QnARecord[] qnARecords = new QnARecord[1];
+            QnARecord qnARecord = new QnARecord();
+            qnARecord.Operation = OperationKind.Add;
+            QnA qnA = new QnA();
+            qnA.QnAId = 0;
+            qnA.Answer = "FirstUpdateQnAs";
+            qnA.SourceName = "Editorial";
+            qnA.Question = new string[] { "FirstUpdateQnAs", "FirstUpdateQnAs" };
+            qnARecord.QnA = qnA;
+            qnARecords[0] = qnARecord;
+
+            var content = JsonConvert.SerializeObject(qnARecords);
+
+            var url = $"{configuration["QKBEndpoint"]}/language/query-knowledgebases/projects/SCAL/qnas?api-version=2021-10-01";
+
+            var JsonResponse = await CallApiEndpoint(url, "patch", content);
+
+            return JsonResponse;
         }
 
         /// <summary>
@@ -70,21 +116,80 @@ namespace CrudCoreDatabaseFirst.Controllers
 
             var url = $"{configuration["QKBEndpoint"]}/language/query-knowledgebases/projects?api-version=2021-10-01";
 
-            var JsonResponse = await CallApiEndpoint(url);
+            var JsonResponse = await CallApiEndpoint(url, "get", string.Empty);
 
             allProjects = JsonConvert.DeserializeObject<ProjectRecords>(JsonResponse);
 
             return allProjects;
         }
 
-        private async Task<string> CallApiEndpoint(string url)
+        /// <summary>
+        /// Wrapper method to call UpdateSourcesFileUpload API endpoint
+        /// </summary>
+        [HttpPatch]
+        [Route("api/UpdateSourcesFileUpload")]
+        public async Task<string> UpdateSourcesFileUpload()
+        {
+            SourceRecord[] newSources = new SourceRecord[1];
+            SourceRecord newSource = new SourceRecord();
+            newSource.Operation = OperationKind.Add;
+            Source source = new Source();
+            source.SourceName = "SCALqnas.xlsx";
+            source.DisplayName = "FirstSSTSource";
+            source.SourceUri = "https://testpdconnect.blob.core.windows.net/users/SCALqnas.xlsx";
+            source.SourceKind = SourceKind.File;
+            newSource.Source = source;
+            newSources[0] = newSource;
+
+            var content = JsonConvert.SerializeObject(newSources);
+
+            var url = $"{configuration["QKBEndpoint"]}/language/query-knowledgebases/projects/test2/sources?api-version=2021-10-01";
+            var JsonResponse = await CallApiEndpoint(url, "patch", content);
+
+            return JsonResponse;
+        }
+
+        /// <summary>
+        /// Wrapper method to call GetSources API endpoint
+        /// </summary>
+        [HttpGet]
+        [Route("api/GetSources")]
+        public async Task<SourceRecords> GetSources()
+        {
+            SourceRecords allSources = new SourceRecords();
+
+            var url = $"{configuration["QKBEndpoint"]}/language/query-knowledgebases/projects/test2/sources?api-version=2021-10-01";
+
+            var JsonResponse = await CallApiEndpoint(url, "get", string.Empty);
+
+            allSources = JsonConvert.DeserializeObject<SourceRecords>(JsonResponse);
+
+            return allSources;
+        }
+
+        /// <summary>
+        /// Common method to call API endpoint
+        /// </summary>
+        private async Task<string> CallApiEndpoint(string url, string operationType, string content)
         {
             var Client = new HttpClient();
             // add QnAAuthKey to Authorization header
             Client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", configuration["QKBApikey"]);
-            var JsonResponse = await Client.GetAsync(url).Result.Content.ReadAsStringAsync();
-
-            return JsonResponse;
+            if (operationType == "get")
+            {
+                return await Client.GetAsync(url).Result.Content.ReadAsStringAsync();
+            }
+            else if (operationType == "patch")
+            {
+                return await Client.PatchAsync(url, new StringContent(content, Encoding.UTF8, "application/json")).Result.Content.ReadAsStringAsync();
+            }
+            else if (operationType == "post")
+            {
+                return await Client.PostAsync(url, new StringContent(content, Encoding.UTF8, "application/json")).Result.Content.ReadAsStringAsync();
+            }
+            else {
+                return string.Empty;
+            }
         }
     }
 }
